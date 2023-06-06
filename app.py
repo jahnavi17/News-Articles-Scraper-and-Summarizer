b927e837b2345915fa9319075f14a63a56f60f85
Jahnavi Innamuri <januinnamuri@gmail.com>
2023-06-06 17:17:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,39 +13,42 @@ st.title("News Articles Scraper and Summarizer")
 st.markdown("## Hey there! You can get Extractive Summary of recent news articles quickly using this web app. You also get the link to the articles so that you can browse those that interest you.")
 input_key = st.text_input("Give the input for which you want to get the news :")
 #input_date = st.text_input("Give the number of days prior you wish to get :")
+#input_key = "Harry Potter"
 
 def get_article_text(link):
     try:
-        link = 'https://'+link
+        link = link
         article = Article(link)
         article.download()
         #print(article)
         article.parse()
         return article.text
     except:
-        return "The article is not accessible"
+        return "This article is not accessible" 
     
 
 def text_summarize_extractive(text):
     parser = PlaintextParser(text , Tokenizer("english"))
     summary =""
     LSASummarizer = Summarizer()
-    num_sentences = 3
+    num_sentences = 4
     for sentence in LSASummarizer(parser.document , num_sentences):
         summary+=(str(sentence))
     return summary
 
-if(input_key) : 
+if(input_key):
     googlenews = GoogleNews("1d")
     googlenews.set_lang("en")
-    googlenews.get_news(input_key)
+    googlenews.search(input_key)
 
-    result = googlenews.result()
+    result = googlenews.results(sort=True)
+
 
     news_db = pd.DataFrame(result)
-    news_db.drop(["desc","datetime","img","site"],axis=1,inplace=True)
+    news_db.drop(["desc","datetime","img"],axis=1,inplace=True)
 
     news_db['Text'] = news_db['link'].apply(get_article_text)
     news_db["Extractive_Summary"] = news_db["Text"].apply(text_summarize_extractive)
-    
+    #news_db.drop(news_db[news_db['Text'] == "This article is not accessible"].index ,inplace=True)
     st.write(news_db)
+#print(news_db.iloc[1])
