116283b21f6a8a7ad5d9f4ec3d1fe84331dab838
Jahnavi Innamuri <januinnamuri@gmail.com>
2023-06-06 14:48:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import requests
-from bs4 import BeautifulSoup as bs
 from GoogleNews import GoogleNews
 from newspaper import Article
 from sumy.parsers.plaintext import PlaintextParser
